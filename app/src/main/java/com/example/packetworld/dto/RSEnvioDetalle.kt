f85adb06f20fb8e18554d7c48aeed79002475081
OhanaMGH <ohanagutierrez229@gmail.com>
2025-12-25 16:30:52 -0600

    Mapear respuesta de detalle de envío en RSEnvioDetalle

---
+++ app/src/main/java/com/example/packetworld/dto/RSEnvioDetalle.kt
@@ -0,0 +1,35 @@
+package com.example.packetworld.dto
+
+import com.example.packetworld.poko.Paquete
+import java.util.List
+
+
+data class RSEnvioDetalle(
+    val idEnvio: Int,
+    val numeroGuia: String,
+    val estatus: String,
+
+    val fechaEnvio: String?,
+    val fechaEntrega: String?,
+
+    val nombreDestinatario: String?,
+    val apellidoPaternoDestinatario: String?,
+    val apellidoMaternoDestinatario: String?,
+
+    val clienteNombre: String?,
+    val clienteApellidoPaterno: String?,
+    val clienteApellidoMaterno: String?,
+    val clienteTelefono: String?,
+    val clienteCorreo: String?,
+
+    val sucursalOrigen: String?,
+
+    val dirCalle: String?,
+    val dirNumero: String?,
+    val dirColonia: String?,
+    val dirCP: String?,
+    val dirCiudad: String?,
+    val dirEstado: String?,
+
+    val paquetes: List<Paquete>?
+)
